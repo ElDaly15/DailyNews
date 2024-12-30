e8e9b7de3c3d7637428f1a364002f4f10c6bc14b
Mazen Eldaly <dalydevelopment15@gmail.com>
2024-12-30 11:27:40 +0200

    Add utility classes for app colors and responsive text styles
    Introduce `AppColors` for defining consistent color themes and `TextStyles` for responsive text styling across the application. The `TextStyles` class includes various font sizes and weights, and utilizes a responsive font size calculation based on the screen width.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Colors.white;
+  static const Color secondaryColorTheme = Color(0xff1877F2);
+  static const Color subTitleColor = Color(0xff4E4B66);
+  static const Color mainTitleColor = Colors.black;
+}
