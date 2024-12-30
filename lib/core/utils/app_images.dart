e9bae9ac57b295293dc7d39c11fc92ee27937a19
Mazen Eldaly <dalydevelopment15@gmail.com>
2024-12-30 11:37:48 +0200

    Add image assets and update pubspec.yaml for launcher icons and splash screen
    - Created `app_images.dart` to manage image assets.
    - Updated `pubspec.yaml` to include `flutter_launcher_icons` and `flutter_native_splash` dependencies.
    - Configured `flutter_icons` and `flutter_native_splash` in `pubspec.yaml` for app icon and splash screen customization.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesIcLauncher
+  /// assets/images/ic_launcher.png
+  static const String imagesIcLauncher = "assets/images/ic_launcher.png";
+
+  /// Assets for imagesNewsLogo
+  /// assets/images/news_logo.png
+  static const String imagesNewsLogo = "assets/images/news_logo.png";
+
+  /// Assets for imagesNewsRemoveLogo
+  /// assets/images/news_remove_logo.png
+  static const String imagesNewsRemoveLogo =
+      "assets/images/news_remove_logo.png";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.png
+  static const String imagesSearch = "assets/images/search.png";
+}
