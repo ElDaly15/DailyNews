588343ff3ad1830f2e8e3f6165c5166c02c4963d
Mazen Eldaly <dalydevelopment15@gmail.com>
2024-12-30 19:14:39 +0200

    Fix incorrect condition for running WorldNewsApp
    The condition to run WorldNewsApp was incorrectly set to only run when not in release mode. This commit corrects the condition to run WorldNewsApp in release mode and use DevicePreview otherwise.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  if (!kReleaseMode) {
+  if (kReleaseMode) {
     runApp(const WorldNewsApp());
   } else {
     runApp(DevicePreview(
